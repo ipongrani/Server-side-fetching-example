a8a7f71dc48edd08d255cacbef2a9a4e174d0195
someone <someone@someplace.com>
2018-09-28 09:44:25 -0700

    Updated

--- routes/Registration.js
+++ routes/Registration.js
@@ -14,11 +14,27 @@ module.exports = (express,axios) => {
     });
 
 
-  Router.post('/newAccount', (req, res, next) => {
+  Router.post('/new_member', (req, res, next) => {
 
     axios({
          method: 'post',
-         url: `http://nucleus-0.herokuapp.com/care-center/Registration?action=new`,
+         url: `http://nucleus-0.herokuapp.com/care-center/Registration?action=nM`,
+         data: {
+           name: req.body.name,
+           address: req.body.address,
+           email: req.body.email,
+           password: req.body.password
+         }
+    }).then((r) => res.send(r.data))
+    .catch((err) => res.send(err.response.data))
+
+  });
+
+  Router.post('/new_facility', (req, res, next) => {
+
+    axios({
+         method: 'post',
+         url: `http://nucleus-0.herokuapp.com/care-center/Registration?action=nF`,
          data: {
            name: req.body.name,
            address: req.body.address,
